898dba9a4ea0f0f0b8d52ac97c1f4734dc5a48f3
Markus Funke <funkemarkus@MacBookPro13.local>
2020-09-25 19:48:16 +0200

    add loop for tranco list

--- selenium/main.py
+++ selenium/main.py
@@ -22,6 +22,7 @@ import os
 import glob
 from pathlib import Path
 import subprocess
+from csv import reader
 
 
 # print(os.path.sep)
@@ -54,68 +55,70 @@ def fetch(url: str, save_path: str):
     logging.info("Closing browser")
     driver.quit()
 
+with open('tranco.csv', 'r') as read_obj:
+    csv_reader = reader(read_obj)
+    for row in csv_reader:
+        url = row[1]
 
-urls = ["google.nl", "nos.nl"]
-for url in urls:
-    folder_path = BASE_DOWNLOAD_FOLDER + [url]
-    jsbeautifier_path = BASE_DOWNLOAD_FOLDER + [url, "jsbeautifier"]
-    purified_path = BASE_DOWNLOAD_FOLDER + [url, "pure"]
-    save_path = folder_path + [url]
-    
-    try:
-        os.mkdir(os.path.sep.join(folder_path))
-    except Exception as e:
-        logging.error(e)
-    try:
-        os.mkdir(os.path.sep.join(jsbeautifier_path))
-    except Exception as e:
-        logging.error(e)
-    try:
-        os.mkdir(os.path.sep.join(purified_path))
-    except Exception as e:
-        logging.error(e)
-
-    fetch("http://%s" % url, os.path.sep.join(save_path))
-
-    jsbeautifiers = []
-    for js_file in glob.glob(os.path.sep.join(save_path) + "**/*.js", recursive=True):
-        file_name = js_file[len(os.path.sep.join(save_path)+"_files/"):]
-        jsbeautifier_file_location = os.path.sep.join(jsbeautifier_path + [file_name])
-        subprocess.call("js-beautify '%s' > %s" % (js_file, jsbeautifier_file_location), shell=True)
-        logging.info("js-beautify '%s' > '%s'" % (js_file, jsbeautifier_file_location))
-        jsbeautifiers.append(jsbeautifier_file_location)
-
-    for js_file in jsbeautifiers:
-        lines_to_delete = []
-        try:
-            stdout = subprocess.check_output("purecheck '%s'" % js_file, shell=True).decode("utf-8")
-            if not stdout.startswith("Impure function"): continue
-            for line in stdout.split("\n"):
-                parts = line[26:].split(")")[0].split(",")
-                if len(parts) != 2: continue
-                lines_to_delete.append(int(parts[0]))
-        except: continue
-
-        js_lines = []
-        with open(js_file, "r") as f: js_lines = f.readlines()
-        for line_number in sorted(set(lines_to_delete), reverse=True):
-            js_lines.pop(line_number-1)
-
-        pure_path = os.path.sep.join(purified_path + [js_file[len(os.path.sep.join(jsbeautifier_path+[""])):]])
+        folder_path = BASE_DOWNLOAD_FOLDER + [url]
+        jsbeautifier_path = BASE_DOWNLOAD_FOLDER + [url, "jsbeautifier"]
+        purified_path = BASE_DOWNLOAD_FOLDER + [url, "pure"]
+        save_path = folder_path + [url]
         
-        with open(pure_path, "w") as f: js_lines = f.writelines(js_lines)
-        logging.info("New pure js file: %s" % pure_path)
-
         try:
-            subprocess.call("rm -rf '%s'" % os.path.sep.join(folder_path + ["%s_files" % url]))
+            os.mkdir(os.path.sep.join(folder_path))
         except Exception as e:
             logging.error(e)
         try:
-            subprocess.call("rm -rf '%s'" % os.path.sep.join(folder_path + ["%s_files" % url]))
+            os.mkdir(os.path.sep.join(jsbeautifier_path))
+        except Exception as e:
+            logging.error(e)
+        try:
+            os.mkdir(os.path.sep.join(purified_path))
         except Exception as e:
             logging.error(e)
 
-        # print("".join(js_lines))
+        fetch("http://%s" % url, os.path.sep.join(save_path))
+
+        jsbeautifiers = []
+        for js_file in glob.glob(os.path.sep.join(save_path) + "**/*.js", recursive=True):
+            file_name = js_file[len(os.path.sep.join(save_path)+"_files/"):]
+            jsbeautifier_file_location = os.path.sep.join(jsbeautifier_path + [file_name])
+            subprocess.call("js-beautify '%s' > %s" % (js_file, jsbeautifier_file_location), shell=True)
+            logging.info("js-beautify '%s' > '%s'" % (js_file, jsbeautifier_file_location))
+            jsbeautifiers.append(jsbeautifier_file_location)
+
+        for js_file in jsbeautifiers:
+            lines_to_delete = []
+            try:
+                stdout = subprocess.check_output("purecheck '%s'" % js_file, shell=True).decode("utf-8")
+                if not stdout.startswith("Impure function"): continue
+                for line in stdout.split("\n"):
+                    parts = line[26:].split(")")[0].split(",")
+                    if len(parts) != 2: continue
+                    lines_to_delete.append(int(parts[0]))
+            except: continue
+
+            js_lines = []
+            with open(js_file, "r") as f: js_lines = f.readlines()
+            for line_number in sorted(set(lines_to_delete), reverse=True):
+                js_lines.pop(line_number-1)
+
+            pure_path = os.path.sep.join(purified_path + [js_file[len(os.path.sep.join(jsbeautifier_path+[""])):]])
+
+            with open(pure_path, "w") as f: js_lines = f.writelines(js_lines)
+            logging.info("New pure js file: %s" % pure_path)
+
+            try:
+                subprocess.call("rm -rf '%s'" % os.path.sep.join(folder_path + ["%s_files" % url]))
+            except Exception as e:
+                logging.error(e)
+            try:
+                subprocess.call("rm -rf '%s'" % os.path.sep.join(folder_path + ["%s_files" % url]))
+            except Exception as e:
+                logging.error(e)
+
+            # print("".join(js_lines))
 
     
 
